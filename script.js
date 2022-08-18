af81821858df695978e279b150a895ab438590cd
Þorvaldur Tumi Baldursson <thorvaldur.tumi@gmail.com>
2022-08-18 13:37:11 +0000

    vinna betur í þessu heima

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 import { getMousePos } from "./lib/utils.js";
 
-let canvas = document.querySelector('canvas');
-let ctx = canvas.getContext('2d');
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
 
-canvas.addEventListener("click", (e) => {
-    let m = getMousePos(canvas, e);
-    ctx.fillStyle = "#000";
-    ctx.fillRect(m.x-5,m.y-5,10,10);
-})
+let dim = { w: canvas.width, h: canvas.height };
+
+let width = 3;
+const widths = [1, 2, 5, 10, 20, 50, 100, 250]
+
+generateGrid(widths[width])
+document.querySelector("#in").addEventListener("click", () => {
+  generateGrid(width < widths.length-1 ? widths[++width] : widths[width]);
+});
+document.querySelector("#out").addEventListener("click", () => {
+  generateGrid(width > 0 ? widths[--width] : widths[width])
+});
+
+function generateGrid(width) {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, dim.w, dim.h);
+  for (let y = 0; y < dim.h; y += width) {
+    for (let x = 0; x < dim.w; x += width) {
+      ctx.fillStyle = getRandomColor();
+      ctx.fillRect(x, y, width, width);
+    }
+  }
+}
+
+
+function getRandomColor() {
+  let val = Math.floor(Math.random() * 255);
+  let out = `rgb(${val},${val},${val})`;
+  return out;
+}
