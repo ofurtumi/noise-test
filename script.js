d421becf896bb293112a8bc9af96f2a790e88e78
Þorvaldur Tumi Baldursson <60275253+ofurtumi@users.noreply.github.com>
2022-08-18 11:57:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+import { getMousePos } from "./lib/utils.js";
+
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+canvas.addEventListener("click", (e) => {
+    let m = getMousePos(canvas, e);
+    ctx.fillStyle = "#000";
+    ctx.fillRect(m.x-5,m.y-5,10,10);
+})
