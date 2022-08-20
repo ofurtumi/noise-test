02d01edd65ff8fd976f2ce7ef198a2f46640f257
ofurtumi <thorvaldur.tumi@gmail.com>
2022-08-20 21:27:58 +0000

    v1.0

--- script.js
+++ script.js
@@ -1,35 +1,80 @@
-import { getMousePos } from "./lib/utils.js";
+import { getMousePos } from './lib/utils.js';
 
-let canvas = document.querySelector("canvas");
-let ctx = canvas.getContext("2d");
-
-let dim = { w: canvas.width, h: canvas.height };
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
 
 let width = 3;
-const widths = [1, 2, 5, 10, 20, 50, 100, 250]
+const widths = [1, 2, 5, 10, 20, 50, 100, 250];
+let grid;
 
-generateGrid(widths[width])
-document.querySelector("#in").addEventListener("click", () => {
-  generateGrid(width < widths.length-1 ? widths[++width] : widths[width]);
+document.querySelector('#in').addEventListener('click', () => {
+	grid = generateGrid(
+		width < widths.length - 1 ? widths[++width] : widths[width]
+	);
+	draw(grid, widths[width], median, color);
 });
-document.querySelector("#out").addEventListener("click", () => {
-  generateGrid(width > 0 ? widths[--width] : widths[width])
+document.querySelector('#out').addEventListener('click', () => {
+	grid = generateGrid(width > 0 ? widths[--width] : widths[width]);
+	draw(grid, widths[width], median, color);
 });
+
+let median = true;
+document.querySelector('#generate').addEventListener('change', () => {
+	median = !median;
+	draw(grid, widths[width], median, color);
+});
+
+let color = true;
+document.querySelector('#color').addEventListener('change', () => {
+	color = !color;
+	draw(grid, widths[width], median, color);
+});
+
+grid = generateGrid(widths[width]);
+draw(grid, widths[width], median, color);
 
 function generateGrid(width) {
-  ctx.fillStyle = "#fff";
-  ctx.fillRect(0, 0, dim.w, dim.h);
-  for (let y = 0; y < dim.h; y += width) {
-    for (let x = 0; x < dim.w; x += width) {
-      ctx.fillStyle = getRandomColor();
-      ctx.fillRect(x, y, width, width);
-    }
-  }
+	let grid = [];
+	for (let y = 0; y < canvas.height; y += width) {
+		let row = [];
+		for (let x = 0; x < canvas.width; x += width) {
+			row.push(Math.round(Math.random() * 100));
+		}
+		grid.push(row);
+	}
+	return grid;
 }
 
+function betterMedian(x, y, grid) {
+	let total = 0;
+	for (let i = -1; i <= 1; i++) {
+		if (grid[y + i]) {
+			for (let j = -1; j <= 1; j++) {
+				if (grid[y + i][x + j]) total += grid[y + i][x + j];
+				else total += Math.round(Math.random() * 100);
+			}
+		} else {
+			total += Math.round(Math.random() * 100) * 3;
+		}
+	}
+	return Math.round(total / 9);
+}
 
-function getRandomColor() {
-  let val = Math.floor(Math.random() * 255);
-  let out = `rgb(${val},${val},${val})`;
-  return out;
+function draw(grid, width, median, color) {
+	ctx.fillStyle = '#fff';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid.length; j++) {
+			if (!median) {
+				ctx.fillStyle = color
+					? 'hsl(' + grid[i][j] * 4 + ',80%,40%)'
+					: 'hsl(0,0%,' + grid[i][j] + '%)';
+			} else {
+				ctx.fillStyle = color
+        	? 'hsl(' + betterMedian(i, j, grid) * 4 + ',80%,40%)'
+					: 'hsl(0,0%,' + betterMedian(i, j, grid) + '%)';
+			}
+			ctx.fillRect(i * width, j * width, width, width);
+		}
+	}
 }
